5e04f58c3ff8ee8ec922117268a9bda88f131466
Jonathan Broster <17BrostJ@fulford.york.sch.uk>
2022-09-21 12:14:56 +0100

    Add project files.

---
+++ Throwaway1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Throwaway1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("English");
+            SayHello("German");
+            SayHello("French");
+
+            
+            
+        }
+        private static void SayHello(string language)
+        {
+            
+
+            switch (language)
+            {
+                case "English":
+                    Console.WriteLine("Hello");
+                    break;
+                case "German":
+                    Console.WriteLine("Guten Tag");
+                    break;
+                case "French":
+                    Console.WriteLine("Bonjour");
+                    break;
+                default:
+                    Console.WriteLine("Ay Up");
+                    break;
+
+            }
+        }
+    }
+}
